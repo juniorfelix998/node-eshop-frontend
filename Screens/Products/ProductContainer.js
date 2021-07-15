f8ad34bb1b90e41f8e456d1d1830ac544f36a236
juniorfelix998 <juniorfelix998@gmail.com>
2021-07-15 16:29:42 +0300

    Added Screens

---
+++ Screens/Products/ProductContainer.js
@@ -0,0 +1,139 @@
+import React, {useEffect, useState} from 'react';
+import {Dimensions, ScrollView, StyleSheet, View} from "react-native";
+import {Container, Header, Icon, Input, Item, Text} from "native-base";
+
+
+import ProductList from './ProductList'
+import SearchedProducts from "./SearchedProducts";
+import Banner from "../../Shared/Banner";
+import CategoryFilter from "./CategoryFilter";
+
+const data = require('../../assets/data/products.json');
+const productsCategories = require('../../assets/data/categories.json')
+
+let {height} = Dimensions.get('window')
+
+const ProductContainer = () => {
+
+    const [products, setProducts] = useState([]);
+    const [productsFiltered, setProductsFiltered] = useState([])
+    const [focus, setFocus] = useState()
+    const [categories, setCategories] = useState([]);
+    const [productsCtg, setProductsCtg] = useState([]);
+    const [active, setActive] = useState();
+    const [initialState, setInitialState] = useState([]);
+
+    useEffect(() => {
+        setProducts(data);
+        setProductsFiltered(data);
+        setFocus(false);
+        setCategories(productsCategories)
+        setProductsCtg(data)
+        setActive(-1)
+        setInitialState(data)
+
+        return () => {
+            setProducts([])
+            setProductsFiltered([])
+            setFocus()
+            setCategories([])
+            setActive()
+            setInitialState()
+        }
+    }, [])
+
+    // Product Methods
+    const searchProduct = (text) => {
+        setProductsFiltered(
+            products.filter((i) => i.name.toLowerCase().includes(text.toLowerCase()))
+        );
+    };
+
+    const openList = () => {
+        setFocus(true);
+    };
+
+    const onBlur = () => {
+        setFocus(false);
+    };
+
+    // Categories
+    const changeCtg = (ctg) => {
+        {
+            "all" === ctg
+                ? [setProductsCtg(initialState), setActive(true)]
+                : [
+                    setProductsCtg(
+                        products.filter((i) => i.category.$oid === ctg),
+                        setActive(true)
+                    ),
+                ];
+        }
+    };
+
+    return (
+        <Container>
+            <Header searchBar rounded>
+                <Item>
+                    <Icon name="ios-search"/>
+                    <Input placeholder="Search" onFocus={openList} onChangeText={(text) => searchProduct(text)}/>
+                    {focus == true ? (<Icon onPress={onBlur} name="ios-close"/>) : null}
+                </Item>
+            </Header>
+            {focus == true ? (<SearchedProducts productsFiltered={productsFiltered}/>) : (<ScrollView>
+                <View>
+
+                    <View>
+                        <Banner/>
+                    </View>
+                    <View>
+                        <CategoryFilter categories={categories} categoryFilter={changeCtg} productsCtg={productsCtg}
+                                        active={active} setActive={setActive}/>
+                    </View>
+
+                    {productsCtg.length > 0 ? (
+                        <View style={styles.listContainer}>
+                            {productsCtg.map((item) => {
+                                return (
+                                    <ProductList
+                                        key={item._id.$oid}
+                                        item={item}
+                                    />
+                                )
+                            })}
+                        </View>
+                    ) : (
+                        <View style={[styles.center, {height: height / 2}]}>
+                            <Text>No products found</Text>
+                        </View>
+                    )}
+
+                </View>
+            </ScrollView>)}
+
+
+        </Container>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexWrap: "wrap",
+        backgroundColor: "gainsboro",
+    },
+    listContainer: {
+        height: height,
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "flex-start",
+        flexWrap: "wrap",
+        backgroundColor: "gainsboro",
+    },
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default ProductContainer
